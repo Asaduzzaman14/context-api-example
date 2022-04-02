e78de821b16f12c6fa3bb8fa36ff028d7d5618ad
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-02 22:21:38 +0600

    comple context API example

---
+++ src/Components/SecondComponent/SecondComponent.js
@@ -0,0 +1,27 @@
+import { GrFormAdd } from "react-icons/gr";
+import { AiOutlineMinus } from "react-icons/ai";
+import { CountContext } from "../../App";
+import { useContext } from "react";
+
+const SecondComponent = () => {
+  const x = useContext(CountContext)
+  const [count, setCount] = x
+  return (
+    <div className='second-component'>
+      <div className='counter-container'>
+        <h1>Second Component</h1>
+        <div>
+          <button title='Increase' onClick={() => setCount(count + 1)} >
+            <GrFormAdd size={20} />
+          </button>
+          <h1>Count:{count} </h1>
+          <button title='Decrease' onClick={() => { setCount(count - 1) }} >
+            <AiOutlineMinus size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondComponent;
---
+++ src/Components/ThirdComponent/ThirdComponent.js
@@ -0,0 +1,28 @@
+import { GrFormAdd } from "react-icons/gr";
+import { AiOutlineMinus } from "react-icons/ai";
+import { useContext } from "react";
+import { CountContext } from "../../App";
+
+const ThirdComponent = () => {
+  const x = useContext(CountContext)
+  const [count, setCount] = x
+
+  return (
+    <div className='third-component'>
+      <div className='counter-container'>
+        <h1>Third Component</h1>
+        <div>
+          <button title='Increase' onClick={() => setCount(count + 1)}>
+            <GrFormAdd size={20} />
+          </button>
+          <h1>Count: {count} </h1>
+          <button title='Decrease' onClick={() => setCount(count - 1)} >
+            <AiOutlineMinus size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThirdComponent;
